be850135e58283343beaf7501d705ee5bc3c435d
WorkingSiege <th3boss057@gmail.com>
2024-05-09 19:17:34 -0500

    Added main

--- Main.py
+++ Main.py
@@ -1,1 +1,15 @@
+from Logistic import *
+from PyQt6.QtWidgets import QApplication
 
+
+def main():
+    app = QApplication([])
+    
+    VoteSystem = MainPage()
+
+    VoteSystem.show()
+
+    app.exec()
+
+if __name__ == "__main__":
+    main()

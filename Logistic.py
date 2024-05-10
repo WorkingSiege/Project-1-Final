34cfa2df10e4ef9cf997282b4f80a73519f4b0f8
WorkingSiege <th3boss057@gmail.com>
2024-05-09 23:22:59 -0500

    Updated main branch with test branch

--- Logistic.py
+++ Logistic.py
@@ -9,12 +9,12 @@ class MainPage(QtWidgets.QMainWindow):
         super().__init__()
         self.ui = Ui_MainPage()
         self.ui.setupUi(self)
-        self.ui.pushButton.clicked.connect(self.openPollAdmin)
+        self.ui.pushButton.clicked.connect(self.PollAdmin)
         self.ui.pushButton_2.clicked.connect(self.saveOptions)
         self.uiPollAdmin = None
         self.VoteReg = {}
 
-    def openPollAdmin(self):                            #Functions as the "Admin" button on the main page, opens PollAdmin panel
+    def PollAdmin(self):                            #Functions as the "Admin" button on the main page, opens PollAdmin panel
         self.uiPollAdmin = QtWidgets.QMainWindow()
         self.uiPollAdminWidget = Ui_Form()
         self.uiPollAdminWidget.setupUi(self.uiPollAdmin)
@@ -22,6 +22,7 @@ class MainPage(QtWidgets.QMainWindow):
 
         if self.VoteReg:
             self.updateTable(self.uiPollAdminWidget, self.VoteReg)
+            self.showPercentage(self.uiPollAdminWidget)
 
 
 
@@ -61,37 +62,55 @@ class MainPage(QtWidgets.QMainWindow):
                 }
             if self.uiPollAdmin is not None:
                 self.updateTable(self.uiPollAdminWidget, self.VoteReg)
-
+                self.showPercentage(self.uiPollAdminWidget)
 
 
     def updateTable(self, uiPollAdmin, dataDict):       #Updates the table displayed in PollAdmin panel
-        tableWidget = uiPollAdmin.tableWidget
+        Database = uiPollAdmin.tableWidget
         keyOrder = ["ID", "Name", "County", "Candidate"]
-        tableWidget.setColumnCount(len(keyOrder))
-        tableWidget.setRowCount(len(dataDict)+1)  
+        Database.setColumnCount(len(keyOrder))
+        Database.setRowCount(len(dataDict)+1)  
 
 
         for col, key in enumerate(keyOrder):
             headerItem = QtWidgets.QTableWidgetItem(key)
-            tableWidget.setItem(0, col, headerItem)
+            Database.setItem(0, col, headerItem)
 
 
         row = 1
         for idNumber, data in dataDict.items():
-            tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(str(idNumber)))
+            Database.setItem(row, 0, QtWidgets.QTableWidgetItem(str(idNumber)))
             for col, key in enumerate(keyOrder[1:], start=1):
                 if key in data:
                     value = data[key]
                     valueItem = QtWidgets.QTableWidgetItem(str(value))
-                    tableWidget.setItem(row, col, valueItem)
+                    Database.setItem(row, col, valueItem)
             row += 1
 
+    def calculatePercentages(self):
+        totalVotes = len(self.VoteReg)
+        if totalVotes == 0:
+            return {}
+        
+        voteCounts = {}
+        for data in self.VoteReg.values():
+            candidate = data["Candidate"]
+            voteCounts[candidate] = voteCounts.get(candidate, 0 ) + 1
 
+        percentages = {}
+        for candidate, votes in voteCounts.items():
+            percentage = (votes/ totalVotes) * 100
+            percentages[candidate] = percentage
+        return percentages
 
+    def showPercentage(self, uiPollAdmin):
+        percentages = self.calculatePercentages()
 
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    mainpage = MainPage()
-    mainpage.show()
-    sys.exit(app.exec())
+        PercentTable = uiPollAdmin.tableWidget_2
+        PercentTable.setColumnCount(len(percentages))
+        PercentTable.setRowCount(2)
+
+        for col, candidate in enumerate(percentages.keys()):
+            PercentTable.setItem(0, col, QtWidgets.QTableWidgetItem(candidate)) 
+            percentage = "{:.0f}%".format(percentages[candidate])
+            PercentTable.setItem(1, col, QtWidgets.QTableWidgetItem(percentage))
